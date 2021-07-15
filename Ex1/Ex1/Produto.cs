1a61a8424409669822c410b2a97ca82b107ec5f5
felipesouza16 <fesouza2345@gmail.com>
2021-07-14 22:33:30 -0300

    tentando arrumar as merda

--- Ex1/Ex1/Produto.cs
+++ Ex1/Ex1/Produto.cs
@@ -3,17 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace Ex1
 {
-    public class Cadastro
+    class Produto
     {
-        public void CadastroProduto()
-        {
-            public int Id { get; set; }
-
-        }
-
     }
 }
-
-
