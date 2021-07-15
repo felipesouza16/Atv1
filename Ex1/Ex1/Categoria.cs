b6d32b47e576691a0f65faaf9fd5d47ac59a7048
felipesouza16 <fesouza2345@gmail.com>
2021-07-15 11:18:41 -0300

    mudanças

--- Ex1/Ex1/Categoria.cs
+++ Ex1/Ex1/Categoria.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Ex1
 {
-    class Categoria
+    public class Categoria
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public Categoria(int id, string nome, string descricao)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Descricao = descricao;
+        }
     }
 }
--- Ex1/Ex1/Produto.cs
+++ Ex1/Ex1/Produto.cs
@@ -5,12 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 namespace Ex1
 {
-    public class Cadastro
+    public class Produto
     {
-        public void CadastroProduto()
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public Produto(int Id, string Nome, decimal Valor)
         {
-            public int Id { get; set; }
-
+            this.Id = Id;
+            this.Nome = Nome;
+            this.Valor = Valor;
         }
 
     }
